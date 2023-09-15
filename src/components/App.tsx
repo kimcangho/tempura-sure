b9da89bf27c0e51182b4759190ced7bf1500e49c
Kent Ho <ho.kimcang@gmail.com>
2023-09-15 18:56:44 -0400

    remove comments

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -36,7 +36,6 @@ const App = () => {
         `https://api.open-meteo.com/v1/forecast?latitude=${LATITUDE}&longitude=${LONGITUDE}&current_weather=true&hourly=temperature_2m&start_date=${currentYear}-${currentMonth}-${startDate}&end_date=${currentYear}-${currentMonth}-${currentDate}&timezone=America%2FNew_York`
       );
       const data = await query.json();
-
       setHourlyDataArr(data.hourly);
       setCurrentTemp(data.current_weather.temperature);
     } catch (err: unknown) {
