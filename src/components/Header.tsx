0dc71ef388ceedfd4f4c36d83da12f0633e805d8
Kent Ho <ho.kimcang@gmail.com>
2023-12-05 12:49:49 -0500

    main page error test in prod

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,14 @@ const Header = (): JSX.Element => {
   return (
     <div className="my-4 w-full text-center">
       <h1 className="font-bold text-3xl">TempuraSure</h1>
+      <button
+        type="button"
+        onClick={() => {
+          throw new Error("Sentry Frontend Error - Main page prod");
+        }}
+      >
+        Throw error
+      </button>
     </div>
   );
 };
